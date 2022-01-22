8c1096dacbaf6ce0dd9b7be08be60e45c46a4b7e
MohamedKamal16 <84516222+MohamedKamal16@users.noreply.github.com>
2022-01-22 18:10:34 +0200

    edit

--- app/src/main/java/com/example/tripplanner/Home/Fragment/Profile_view.java
+++ app/src/main/java/com/example/tripplanner/Home/Fragment/Profile_view.java
@@ -12,6 +12,44 @@ import com.example.tripplanner.R;
 public class Profile_view extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+<<<<<<< Updated upstream
         return inflater.inflate(R.layout.fragment_profile,container,false);
+=======
+
+        View view=inflater.inflate(R.layout.fragment_profile,container,false);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        return view;
+
+    }
+    public void setprofile()
+    {
+        user= firebaseAuth.getCurrentUser();
+        String userId=user.getUid();
+        databaseReference= FirebaseDatabase.getInstance().getReference("users").child(userId);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists())
+                {
+                    HashMap<String,String> hashMap=(HashMap<String, String>)snapshot.getValue();
+                    String userName=hashMap.get("username");
+                    String Email=hashMap.get("Email");
+                    pbinding.tvUsernameprofile.setText(userName);
+                    pbinding.tvEmailprofile.setText(Email);
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+             //   Toast.makeText(Profile_view.this,"Erorr No data on to profile");
+
+            }
+        });
+
+>>>>>>> Stashed changes
     }
 }
